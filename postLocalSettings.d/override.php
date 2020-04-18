a770fb6c4ee5d2601908f28082f8344c214ee9b4
Greg Rundlett <greg@freephile.com>
2020-04-18 18:27:32 -0400

    remove wgSpamRegex

--- postLocalSettings.d/override.php
+++ postLocalSettings.d/override.php
@@ -26,29 +26,8 @@ $wgNamespacesWithSubpages = array(
 );
 
 
-// eliminate spam https://www.mediawiki.org/wiki/Manual:$wgSpamRegex
-// may need to adjust 
-// Perl Compatible Regular Expressions backtrack memory limit
 ini_set( 'pcre.backtrack_limit', '2M' );
-$wgSpamRegex = "/".                        # The "/" is the opening wrapper
-                "s-e-x|zoofilia|sexyongpin|grusskarte|geburtstagskarten|animalsex|".
-                "sex-with|dogsex|adultchat|adultlive|camsex|sexcam|livesex|sexchat|".
-                "chatsex|onlinesex|adultporn|adultvideo|adultweb.|hardcoresex|hardcoreporn|".
-                "teenporn|xxxporn|lesbiansex|livegirl|livenude|livesex|livevideo|camgirl|".
-                "spycam|voyeursex|casino-online|online-casino|kontaktlinsen|cheapest-phone|".
-                "laser-eye|eye-laser|fuelcellmarket|lasikclinic|cragrats|parishilton|".
-                "paris-hilton|paris-tape|2large|fuel-dispenser|fueling-dispenser|huojia|".
-                "jinxinghj|telematicsone|telematiksone|a-mortgage|diamondabrasives|".
-                "reuterbrook|sex-plugin|sex-zone|lazy-stars|eblja|liuhecai|".
-                "buy-viagra|-cialis|-levitra|boy-and-girl-kissing|". # These match spammy words
-                "dirare\.com|".           # This matches dirare.com a spammer's domain name
-                "overflow\s*:\s*auto|".   # This matches against overflow:auto (regardless of whitespace on either side of the colon)
-                "height\s*:\s*[0-4]px|".  # This matches against height:0px (most CSS hidden spam) (regardless of whitespace on either side of the colon)
-                "==<center>\[|".          # This matches some recent spam related to starsearchtool.com and friends
-                "/i";                     # The "/" ends the regular expression and the "i" switch which follows makes the test case-insensitive
-                                          # The "\s" matches whitespace
-                                          # The "*" is a repeater (zero or more times)
-                                          # The "\s*" means to look for 0 or more amount of whitespace
+$wgSpamRegex = "";
 
 // add feature to automatically use 'logo' file
 // Use this wiki's logo if it has Logo.png or Logo.jpg uploaded
